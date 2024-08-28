f07d107de35a6ae152942dd4bee581e43f91517b
Zaid <zaidu2020@gmail.com>
2024-08-28 18:33:10 +0530

    First commit

---
+++ main.py
@@ -0,0 +1,102 @@
+# print("hello Anas")
+
+# Write a program that is a simple calculator for sum
+# user input and user can give you multiple values and you program shold give the sum of it
+
+# def sum_calulator():
+#     user_input = input("Enter your number: ")
+#     numbers = user_input.split()
+#     total = sum(map(float, numbers))
+#     print(f"The sum of the entered number is {total}")
+
+# sum_calulator()
+# num1 = float(input("Enter the first number: "))
+# num2 = float(input("Enter the first number: "))
+
+# sum_result = num1 + num2
+# diffrence_result = num1 - num2
+# product_result = num1 * num2
+# quotient_reslut = num1 / num2
+
+# print(f"sum: {sum_result}")
+# print(f"diffrence: {diffrence_result}")
+# print(f"product: {product_result}")
+# print(f"quotient: {quotient_reslut}")
+
+
+#Write a Python program to check if a given number is even or odd.
+# num = int(input("Enter your number: "))
+
+# if num % 2 == 0:
+#   print(f"{num} is even")
+
+# else :
+#   print("num is odd")
+
+#Write a Python program that takes three numbers as input and prints the largest of them.
+# num1 = float(input("Enter the first number: "))
+# num2 = float(input("Enter the first number: "))
+# num3 = float(input("Enter the first number: "))
+
+# if num1 >= num2 and num1 >= num3:
+#   largest = num1
+
+# elif num2 >= num1 and num2 >= num3:
+#   largest = num2
+
+# else:
+#   largest = num3
+
+# print(f"The Largest number is {largest}")
+
+#Write a Python program to reverse a string entered by the user.
+# Name = input("Enter your name: ")
+# reversed_name = Name[::-1]
+
+# print(f"Reversed name is : {reversed_name}")
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name 
+#         self.age = age
+
+#     def greet(self):
+#         print(f"Hello my name is {self.name} and my age is {self.age}.")
+
+# p = Person("Alfa", 29)
+# p.greet()
+
+# from datetime import datetime
+
+# class Car:
+#     def __init__(self,make, model, year=None):
+#         self.make = make
+#         self.model = model 
+#         self.year = year if year is not None else datetime.now().year
+
+#     def description(self):
+#         print(f"Car:{self.model} {self.make} {self.year}")
+
+# c = Car("I20", "Hyundai")
+# c.description()
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# project2
